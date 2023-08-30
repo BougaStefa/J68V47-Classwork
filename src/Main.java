d0a731c1ab8fd0a6e1a3ac849266a7d98ea4a6bc
30216829 <30216829@nescol.ac.uk>
2023-08-30 15:07:12 +0100

    Added Hello World program

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * My first program
+ * @author Stefanos Bougatsas
+ * @version 1
+ */
+public class Main {
+    public static void main(String[] args) {
+        // Write out Hello World to the console
+        System.out.println("Hello World");
+            }
+        }
