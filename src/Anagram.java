03b32bec5e1c78931face25a5a07800f5ee3975d
Stefanos Bougatsas <bougastefa@gmail.com>
2023-09-05 22:46:37 +0100

    Anagram and Haiku exercises

---
+++ src/Anagram.java
@@ -0,0 +1,11 @@
+public class Anagram {
+    public static void main(String[] args)
+    {
+        System.out.println("Dormitory = Dirty room");
+        System.out.println("Elephant = Than Sleep");
+        System.out.println("Abandoned = Bad And One");
+        System.out.println("Earliness = Sale Rinse");
+        System.out.println("Fabricate = A Car Befit");
+        System.out.println("Pacemaker = Cream Peak");
+    }
+}
---
+++ src/Haiku.java
@@ -0,0 +1,7 @@
+public class Haiku {
+    public static void main(String[] args) {
+        System.out.println("Gentle drops descend,");
+        System.out.println("Nature's tears from heavens weep,");
+        System.out.println("Life renewed by rain.");
+    }
+}
