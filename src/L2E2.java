50300ea9a90f782face7d49d9e2ccd0097a8c619
30216829 <30216829@nescol.ac.uk>
2023-09-06 15:31:59 +0100

    Completed in class

---
+++ src/L2E2.java
@@ -0,0 +1,26 @@
+/**
+ * Take 2 numbers, perform and display calculations using them
+ */
+
+import java.util.*;
+public class L2E2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Provide the first number: ");
+        int firstNum = input.nextInt();
+        System.out.print("Provide the second number: ");
+        int secondNum = input.nextInt();
+        int addition = firstNum + secondNum;
+        int subtraction = firstNum - secondNum;
+        int multiplication = firstNum * secondNum;
+        int division = firstNum / secondNum;
+        int modulo = firstNum % secondNum;
+        int exponent = (int) Math.pow(firstNum, secondNum);
+        System.out.println(firstNum   +   "+" +   secondNum   +   "=" +   addition);
+        System.out.println(firstNum   +   "-" +   secondNum   +   "=" +   subtraction);
+        System.out.println(firstNum   +   "*" +   secondNum   +   "=" +   multiplication);
+        System.out.println(firstNum   +   "/" +   secondNum   +   "=" +   division);
+        System.out.println(firstNum   +   "%" +   secondNum   +   "=" +   modulo);
+        System.out.println(firstNum   +   "^" +   secondNum   +   "=" +   exponent);
+    }
+}
