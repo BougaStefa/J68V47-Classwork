a376f183302e73d064186fa2967ac28c65b137fe
30216829 <30216829@nescol.ac.uk>
2023-09-06 13:09:39 +0100

    Fixed typo

--- src/Anagram.java
+++ src/Anagram.java
@@ -2,7 +2,7 @@ public class Anagram {
     public static void main(String[] args)
     {
         System.out.println("Dormitory = Dirty room");
-        System.out.println("Elephant = Than Sleep");
+        System.out.println("Elephants = Than Sleep");
         System.out.println("Abandoned = Bad And One");
         System.out.println("Earliness = Sale Rinse");
         System.out.println("Fabricate = A Car Befit");
