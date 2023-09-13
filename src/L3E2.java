76b2659b1661ea9a929f28b88a87af781cf6fde6
30216829 <30216829@nescol.ac.uk>
2023-09-13 12:38:19 +0100

    Exercise 2 , completed in class

---
+++ src/L3E2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class L3E2 {
+    public static void main(String[] args) {
+        Scanner input   =   new Scanner(System.in);
+        System.out.println("What is the capital of Spain");
+        String answer1  =   input.nextLine().toUpperCase();
+        System.out.println(answer1.contains("MADRID"));
+        System.out.println("What is the capital of the UK");
+        String answer2  =   input.nextLine().toUpperCase();
+        System.out.println(answer2.contains("LONDON"));
+        System.out.println("What is the capital of Italy");
+        String answer3  =   input.nextLine().toUpperCase();
+        System.out.println(answer3.contains("ROME"));
+    }
+}
