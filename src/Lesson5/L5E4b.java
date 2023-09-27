339f52271cc4dcce7451d861909b27b3f26eb60b
30216829 <30216829@nescol.ac.uk>
2023-09-27 14:46:04 +0100

    Finished in class

---
+++ src/Lesson5/L5E4b.java
@@ -0,0 +1,24 @@
+package Lesson5;
+import java.util.*;
+public class L5E4b {
+    public static void main(String[] args) {
+        Scanner input   =   new Scanner(System.in);
+        Random  random  =   new Random();
+        int     corNum  =   random.nextInt();
+        String  result  =   "";
+        for(int count   =   0;  count<5;    count++){
+            System.out.println("Guess the number");
+            int guess   =   input.nextInt();
+            if(guess    ==  corNum) {
+                result  = "Correct!!";
+                count = 5;
+            }
+            else{
+                result  =   "Wrong!!";
+
+            }
+        }
+        System.out.println(result);
+    }
+}
+
