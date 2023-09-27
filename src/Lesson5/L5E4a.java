ead27c91d9e62b2861026050ddb35018c78664d9
30216829 <30216829@nescol.ac.uk>
2023-09-27 14:30:15 +0100

    Finished in class

---
+++ src/Lesson5/L5E4a.java
@@ -0,0 +1,14 @@
+package Lesson5;
+import java.util.*;
+public class L5E4a {
+    public static void main(String[] args) {
+        Scanner input   =   new Scanner(System.in);
+        System.out.println("Enter a number");
+        int     n       =   input.nextInt();
+        long     fact    =   1;
+        for(int count   =   n;  count>=1;   count--){
+            fact       *=   count;
+        }
+        System.out.println("The factorial of "  +   n   +   " is "   +  fact);
+    }
+}
