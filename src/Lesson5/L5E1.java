acddee149b435354e997f1c3f93968ebfa558aa1
30216829 <30216829@nescol.ac.uk>
2023-09-27 12:55:08 +0100

    Finished in class

---
+++ src/Lesson5/L5E1.java
@@ -0,0 +1,13 @@
+package Lesson5;
+import java.util.*;
+public class L5E1 {
+    public static void main(String[] args) {
+        Scanner input   =   new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num         =   input.nextInt();
+        for(int count=0;    count<13;   count++)
+        {
+            System.out.println(num*count);
+        }
+    }
+}
