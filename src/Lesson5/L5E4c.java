9f5cb83b6f5946dd4699a5f05133283c3833141a
Stefanos Bougatsas <bougastefa@gmail.com>
2023-10-03 23:31:00 +0100

    Completed at home

---
+++ src/Lesson5/L5E4c.java
@@ -0,0 +1,18 @@
+package Lesson5;
+
+import java.util.*;
+
+public class L5E4c {
+    public static void main(String[] args) {
+        Scanner input   =   new Scanner(System.in);
+        System.out.println("Enter the number for the pattern");
+        int pat         =   input.nextInt();
+        System.out.println("Enter the length of the pattern");
+        int leng        =   input.nextInt();
+        for(int i=0;    i<leng;    i++) {
+            System.out.print("*" + pat + "*.");
+
+        }
+        }
+    }
+
