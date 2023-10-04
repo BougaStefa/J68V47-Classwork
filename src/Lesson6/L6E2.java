52b1079813c07ebfc0f2e4d5d5bad31387a31eab
30216829 <30216829@nescol.ac.uk>
2023-10-04 14:16:14 +0100

    Finished in class

--- src/Lesson6/L6E2.java
+++ src/Lesson6/L6E2.java
@@ -10,6 +10,7 @@ public class L6E2 {
                     2.Tell me the time
                     3.Tell me a joke
                     4.Quit""");
+            //text block recommended, initially had \n but this looks cleaner
             ans =   input.nextInt();
         }while(ans!=4);
         input.close();
