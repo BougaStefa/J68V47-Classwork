135a8287b769af023d720a35540a881278bcb525
30216829 <30216829@nescol.ac.uk>
2023-10-04 14:15:34 +0100

    Finished in class

---
+++ src/Lesson6/L6E2.java
@@ -0,0 +1,17 @@
+package Lesson6;
+import java.util.Scanner;
+public class L6E2 {
+    public static void main(String[] args) {
+        Scanner input   =   new Scanner(System.in);
+        int ans;
+        do {
+            System.out.println("""
+                    1.Say"Hello"
+                    2.Tell me the time
+                    3.Tell me a joke
+                    4.Quit""");
+            ans =   input.nextInt();
+        }while(ans!=4);
+        input.close();
+    }
+}
