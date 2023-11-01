9e763cf05ed5747921bd414ad68da2af497fcdbf
30216829 <bougastefa@gmail.com>
2023-11-01 10:52:07 +0000

    Finished in class.

---
+++ src/Lesson8/L8E1.java
@@ -0,0 +1,17 @@
+package Lesson8;
+import java.util.Scanner;
+
+public class L8E1 {
+    public static void main(String[] args) {
+        String[] names = new String[5];
+        Scanner input = new Scanner(System.in);
+        for(int i=0; i<=4; i++){
+            System.out.println("Enter the name of student number " + (i+1));
+            names[i] = input.nextLine();
+        }
+        System.out.printf("Out of %d students the first one is %s and the last one is %s",names.length,names[0],names[4]);
+
+
+
+    }
+}
