b044bc94e93c33f8e8ffda41788a60c136b0b134
Stefanos Bougatsas <bougastefa@gmail.com>
2023-11-30 16:34:01 +0000

    Refactored conditional statements for test score grading

--- src/Lesson4/L4E4b.java
+++ src/Lesson4/L4E4b.java
@@ -2,26 +2,22 @@ package Lesson4;
 
 import java.util.*;
 
-import static org.fusesource.jansi.Ansi.*;
-import static org.fusesource.jansi.Ansi.Color.*;
-
 public class L4E4b {
 		public static void main(String[] args) {
 				Scanner input = new Scanner(System.in);
 				System.out.println("Enter the % of the student's test score");
 				int score = input.nextInt();
 				if (score >= 70) {
-						System.out.println(ansi().eraseScreen().fg(GREEN).a("Congratulations, you got an A"));
+						System.out.println("Congratulations, you got an A");
 				} else if (score >= 60) {
-						System.out.println(ansi().eraseScreen().fg(GREEN).a("Congratulations, you got a B"));
+						System.out.println("Congratulations, you got a B");
 				} else if (score >= 50) {
-						System.out.println(ansi().eraseScreen().fg(YELLOW).a("Congratulation, you got a C"));
+						System.out.println("Congratulation, you got a C");
 				} else if (score >= 40) {
-						System.out.println(ansi().eraseScreen().fg(YELLOW).a("Congratulations, you got a D"));
+						System.out.println("Congratulations, you got a D");
 				} else {
-						System.out.println(ansi().eraseScreen().fg(RED).a("Sorry, you get no award"));
+						System.out.println("Sorry, you get no award");
 				}
-
-
+				input.close();
 		}
 }
